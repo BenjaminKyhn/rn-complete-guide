bc1933a233183498e2473daed4683a489d16fb5b
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-18 16:28:23 +0100

    Styles variable

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.screen}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput
-            placeholder="Course goal"
-            style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}/>
-        <Button title="ADD"/>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Course goal"
+                    style={styles.input}/>
+                <Button title="ADD"/>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50
-  }
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    input: {
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+    }
 });
