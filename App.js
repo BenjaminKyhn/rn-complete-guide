f4021b5edb27f6f0c441b153c3e68813ed99401d
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-18 17:06:49 +0100

    ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, ScrollView} from 'react-native';
 
 export default function App() {
     const [enteredGoal, setEnteredGoal] = useState('');
@@ -25,9 +25,13 @@ export default function App() {
                 />
                 <Button title="ADD" onPress={addGoalHandler}/>
             </View>
-            <View>
-                {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
-            </View>
+            <ScrollView>
+                {courseGoals.map(goal => (
+                    <View key={goal} style={styles.listItem}>
+                        <Text>{goal}</Text>
+                    </View>
+                ))}
+            </ScrollView>
         </View>
     );
 }
@@ -46,5 +50,12 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth: 1,
         padding: 10,
+    },
+    listItem: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: '#ccc',
+        borderColor: 'black',
+        borderWidth: 1,
     }
 });
